64e094432576454f43a78e7498632cdcd060e01f
BitO BSessiOn <bitobsession@gmail.com>
2015-11-12 11:50:18 +0100

    Fix for some device building

--- media/libstagefright/mpeg2ts/Android.mk
+++ media/libstagefright/mpeg2ts/Android.mk
@@ -14,7 +14,8 @@ LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/$(call project-path-for,qcom-media)/mm-core/inc
 
-LOCAL_CFLAGS += -Werror
+LOCAL_CFLAGS +=
+#-Werror
 LOCAL_CLANG := true
 LOCAL_SANITIZE := unsigned-integer-overflow
 
